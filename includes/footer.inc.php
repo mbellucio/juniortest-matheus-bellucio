785baba83cee7a98250ba6f9d133b8bf0217c2f4
Matheus <=>
2023-04-16 18:10:46 -0300

    add header and footer, include bootstrap and other tools

---
+++ includes/footer.inc.php
@@ -0,0 +1,17 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+</body>
+
+<footer>
+
+  <div class="footer">
+    <div class="rule-box">
+      <hr class="rule">
+      <span class="footer-text" >Scandiweb test assignment</span>
+    </div>
+    <div class="footer-end"></div>
+  </div>
+
+</footer>
+
+</html>
