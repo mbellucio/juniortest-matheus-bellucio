08d6015fcfa69485812ea3d50b34a76e67273954
Matheus <=>
2023-04-16 15:11:49 -0300

    complete the controller tests

--- test.php
+++ test.php
@@ -1,8 +1,9 @@
 <?php
+    include "includes/class-autoloader.inc.php"; 
 
     $productTypeString = $_POST['producttype']; 
     $productType = new ReflectionClass($productTypeString);
     $productTypeInstance = $productType->newInstance(); 
     $controller = new ProductController();  
-    $controller->addProduct($productTypeInstance, "pdct22896", "chair", 220, 19, 25, 32); 
+    $controller->addProduct($productTypeInstance, "pdct2489", "DVD", 20, 200); 
 
