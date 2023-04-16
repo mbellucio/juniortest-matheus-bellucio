51ae033af7d697552c39a6d1ade43852d34fbcf0
Matheus <=>
2023-04-16 19:11:42 -0300

    add addproduct.php page

---
+++ addproduct.php
@@ -0,0 +1,24 @@
+<?php
+  include "includes/header.inc.php";     
+?>
+
+<div class="container">
+  <div class="row align-items-end">
+      <div class="col">
+        <h2 class="page-title">Product Add</h2>
+      </div>
+      <div class="col btn-box text-end">
+        <button type="button" class="menu-buttons btn btn-dark">Save</button>
+        <a type="button" class="menu-buttons btn btn-dark" onclick="href='index.php'">Cancel</a>
+      </div>
+  </div>
+</div>
+
+<div class="rule-box">
+  <hr class="rule">
+</div>
+
+
+<?php
+  include "includes/footer.inc.php";     
+?>
