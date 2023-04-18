084e8fc6fcc96178ee7610d66cb9ee2e2ab7c4ea
Matheus <=>
2023-04-18 01:07:01 -0300

    remove test files

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include "includes/class-autoloader.inc.php"; 
-
-    $productTypeString = $_POST['producttype']; 
-    $productType = new ReflectionClass($productTypeString);
-    $productTypeInstance = $productType->newInstance(); 
-    $controller = new ProductController();  
-    $controller->addProduct($productTypeInstance, "jvc205", "ACME Disc", 2, 300); 
-
