42cc5c568dd10f04429ab35e54dd24be50d4e451
Matheus <=>
2023-04-18 21:12:54 -0300

    fix autoloader function

--- includes/class-autoloader.inc.php
+++ includes/class-autoloader.inc.php
@@ -4,14 +4,15 @@
 
   function myAutoLoader($classname) {
     $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-
+  
     if (strpos($url, 'includes') !== false) {
       $path = '../';
     } else {
       $path = ''; 
     }
-
+  
     $extension = ".class.php";
+    $classname = str_replace('\\', '/', $classname); 
     require_once $path . $classname . $extension; 
   }
 
